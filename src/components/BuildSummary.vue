e3c84bee8dac534b3770964da8b4ea3612988de4
Jibin M <jmathew1991@gmail.com>
2018-03-16 08:47:25 -0400

    Added images

--- src/components/BuildSummary.vue
+++ src/components/BuildSummary.vue
@@ -6,14 +6,16 @@
         <i class="fa fa-angle-left"></i><span>Change Your Crate</span>
       </button>
       <div class="BuildSummary__product">
+        <img :src="mainProduct | getProductImage" alt="">
         <span class="BuildSummary__product-title">{{mainProduct.title}}</span>
         <span class="BuildSummary__product-size"></span>
-        <span class="BuildSummary__product-price"></span>
+        <span class="BuildSummary__product-price">{{mainProduct |moneyFormat}}</span>
       </div>
     </div>
     <ul class="BuildSummary__addon-products">
       <li v-for="addon in addonProducts">
         <div class="BuildSummary__product">
+          <img :src="addon | getProductImage" alt="">
           <span class="BuildSummary__product-title">{{addon.title}}({{addon.quantity}})</span>
           <span class="BuildSummary__product-size"></span>
           <span class="BuildSummary__product-price">{{addon | moneyFormat}}</span>
@@ -22,6 +24,7 @@
       </li>
     </ul>
     <div class="BuildSummary__card-product">
+      <img :src="cardProduct | getProductImage" alt="">
       {{cardProduct.title}} - {{cardProduct | moneyFormat}}
       {{cardProduct.message}}
     </div>
